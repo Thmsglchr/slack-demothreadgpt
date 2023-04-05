f5bf1aa84522b724434b22dc5d3dda697cd8f62d
Thomas <thomas.guilcher@hotmail.fr>
2023-04-05 11:47:09 +0200

    Update 2023-04-05

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ const limiter = rateLimit({
   max: 1, // limit each IP to 1 request per windowMs
 });
 
+// Apply the rate limiter to the chat.postMessage route
+app.client.post('/chat.postMessage', limiter);
+
 // Store added users
 let addedUsers = [];
 
